60d0e45644da711b455eb30cdf8529c239dc49f7
rich-tribeweb <rich@tribeweb.co.uk>
2024-08-06 08:24:36 +0100

    feat: new logo

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -10,7 +10,7 @@ const { header } = useAppConfig()
   <UHeader>
     <template #logo>
       <template v-if="header?.logo?.dark || header?.logo?.light">
-        <UColorModeImage v-bind="{ class: 'h-12 w-auto', ...header?.logo }" />
+        <UColorModeImage v-bind="{ class: 'h-10 w-auto', ...header?.logo }" />
         <span class="font-feature text-4xl text-primary tracking-[-0.035em] font-bold">tribeweb</span>
       </template>
       <template v-else>
